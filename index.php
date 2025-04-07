1d029f6f31f8e3ffdddaf4a9f6b3d8954c0e33fd
Linda Johanna <jcol_10@outlook.com>
2025-04-06 23:16:06 -0500

    Colocar el titulo en negrila y subrayado en amarillo

--- index.php
+++ index.php
@@ -1,5 +1,9 @@
 <form action="#" method="post">
-    <h2><em>Formulario de registro<br>Universidad de La Salle</em></h2>
+    <h2>
+        <span style="background-color: yellow; font-weight: bold; text-decoration: underline;">
+            Formulario de registro<br>Universidad de La Salle
+        </span>
+    </h2>
     <input type="text" name="nombre" placeholder="Nombre" required>
     <input type="email" name="correo" placeholder="Correo" required>
     <input type="text" name="telefono" placeholder="Teléfono" required>
@@ -8,5 +12,6 @@
         <input type="reset" value="Limpiar">
     </div>
 </form>
+
 
 
